63824768e8719444e8d67eae97ce3aa6c9be659d
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2025-07-07 12:44:35 -0500

    feat: Neider Lizcano - ejercicio 2 taller 3

---
+++ src/main/java/Taller3_Ejercicios/Matematicas.java
@@ -0,0 +1,25 @@
+package Taller3_Ejercicios;
+
+/*Ejercicio 2: Clase Matematicas con Métodos static
+
+ 1. Define una clase Matematicas que tenga métodos static para realizar las operaciones básicas (suma, resta, multiplicación, división).
+ 2. Crea una clase de prueba que llame a estos métodos y muestre los resultados*/
+public class Matematicas {
+
+    public static int Sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static int Restar(int a, int b) {
+        return a - b;
+    }
+
+    public static int Multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static double Dividir(int a, int b) {
+        return a / b;
+    }
+
+}
---
+++ src/main/java/Taller3_Ejercicios/PruebaEjercicioMatematicas.java
@@ -0,0 +1,13 @@
+
+package Taller3_Ejercicios;
+
+public class PruebaEjercicioMatematicas {
+
+    public static void main(String[] args) {
+        System.out.println("Suma: " + Matematicas.Sumar(2, 9));
+        System.out.println("Resta: " + Matematicas.Restar(10, 6));
+        System.out.println("Multiplicación: " + Matematicas.Multiplicar(3, 5));
+        System.out.println("División: " + Matematicas.Dividir(8, 3));
+    }
+}
+
