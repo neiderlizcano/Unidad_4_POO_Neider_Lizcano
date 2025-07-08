084f7e6ff0eec860fe5bd2928dd8b2b31d629449
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2025-07-07 21:34:00 -0500

    feat: Neider Lizcano - Ejercicio 2 Taller 10; se crean las clases Vehiculo y Bicicleta. Bicicleta sobrescribe el método moverse y se demuestra la sobrescritura en la clase Prueba_vehiculo_bicicleta.

---
+++ src/main/java/Taller10_Ejercicios/Vehiculo.java
@@ -0,0 +1,14 @@
+
+package Taller10_Ejercicios;
+
+/**
+Ejercicio 2: Clase Vehiculo y Clase Bicicleta
+ 1. Define una clase Vehiculo con un método moverse que imprima un mensaje general.
+ 2. Define una clase Bicicleta que sobrescriba el método moverse para mostrar un mensaje específico.
+ 3. Crea una clase de prueba para instanciar objetos de Vehiculo y Bicicleta y demostrar la sobrescritura de métodos.
+ */
+public class Vehiculo {
+    public void moverse() {
+        System.out.println("El vehículo se está moviendo.");
+    }
+}
