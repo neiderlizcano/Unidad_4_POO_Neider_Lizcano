21d3865175d40dbee7f2da3b6574a41aae5f53a5
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2025-07-08 14:24:36 -0500

    feat: Neider Lizcano - Ejercicio 2 Taller 14; se crean las interfaces Volador y Nadador, y la clase Pato que implementa ambas. En la clase de prueba Prueba_pato_nadador_volador se demuestra cómo el objeto pato puede volar y nadar usando los métodos definidos.

---
+++ src/main/java/Taller14_Ejercicios/Pato.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller14_Ejercicios;
+
+/**
+Ejercicio 2: Interfaces Volador y Nadador
+ 1. Define una interfaz Volador con un método volar.
+ 2. Define una interfaz Nadador con un método nadar.
+ 3. Crea una clase Pato que implemente ambas interfaces y defina sus métodos.
+ 4. Crea una clase de prueba para instanciar un objeto de Pato y usar ambos métodos
+ */
+public class Pato implements Volador, Nadador {
+
+    @Override
+    public void volar() {
+        System.out.println("El pato está volando bajo.");
+    }
+
+    @Override
+    public void nadar() {
+        System.out.println("El pato nada en el lago.");
+    }
+}
+
---
+++ src/main/java/Taller14_Ejercicios/Prueba_pato_nadador_volador.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller14_Ejercicios;
+
+/**
+Ejercicio 2: Interfaces Volador y Nadador
+ 1. Define una interfaz Volador con un método volar.
+ 2. Define una interfaz Nadador con un método nadar.
+ 3. Crea una clase Pato que implemente ambas interfaces y defina sus métodos.
+ 4. Crea una clase de prueba para instanciar un objeto de Pato y usar ambos métodos
+ */
+public class Prueba_pato_nadador_volador {
+    public static void main(String[] args) {
+        Pato pato = new Pato();
+        pato.volar();
+        pato.nadar();
+    }
+}
+
