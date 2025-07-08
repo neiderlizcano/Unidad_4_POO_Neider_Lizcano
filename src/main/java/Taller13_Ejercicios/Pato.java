e7c83b72a1ec00199b001fddb12eb869df59fe8e
Neider Lizcano <nlizcanos@unicartagena.edu.co>
2025-07-08 10:51:16 -0500

    feat: Neider Lizcano - Ejercicio 2 Taller 13; se crean las interfaces Volador y Nadador, y la clase Pato que implementa ambas. En la clase de prueba Prueba_pato_nadador_volador se demuestra el uso de los métodos volar y nadar desde un solo objeto.

---
+++ src/main/java/Taller13_Ejercicios/Pato.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller13_Ejercicios;
+
+/**
+ *
+ * @author EA440 Operator
+ */
+public class Pato implements Volador, Nadador {
+
+    @Override
+    public void volar() {
+        System.out.println("El pato está volando.");
+    }
+
+    @Override
+    public void nadar() {
+        System.out.println("El pato nada en el lago.");
+    }
+}
+
---
+++ src/main/java/Taller13_Ejercicios/Prueba_pato_nadador_volador.java
@@ -0,0 +1,16 @@
+package Taller13_Ejercicios;
+
+/**
+Ejercicio 2: Interfaces Volador y Nadador
+ 1. Define una interfaz Volador con un método volar.
+ 2. Define una interfaz Nadador con un método nadar.
+ 3. Crea una clase Pato que implemente ambas interfaces y defina sus métodos.
+ 4. Crea una clase de prueba para instanciar un objeto de Pato y usar ambos métodos.
+ */
+public class Prueba_pato_nadador_volador {
+    public static void main(String[] args) {
+        Pato pato = new Pato();
+        pato.volar();
+        pato.nadar();
+    }
+}
